c584551c4647366b5576485dc6a8e726b60f7d72
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2017-11-10 16:34:22 +1100

    Added default value into size table when pivoting as NaN would appear if a user deleted all their storage in a project.

--- UsageDataset.py
+++ UsageDataset.py
@@ -205,7 +205,7 @@ class ProjectDataset(object):
 
         # Pivot makes columns of all the individuals, rows are indexed by date
         try:
-            df = pd.read_sql_query(qstring.format(namefield=name_sql,datafield=datafield,table=table,start=startdate,end=enddate), self.db.executable).pivot(index='Date',columns='Name')
+            df = pd.read_sql_query(qstring.format(namefield=name_sql,datafield=datafield,table=table,start=startdate,end=enddate), self.db.executable).pivot_table(index='Date',columns='Name',fill_value=0)
         except:
             print("No data available for {}".format(storagept))
             return None
